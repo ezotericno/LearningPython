f09274782f2e4d46864cea37a36a9d3534a3a618
ezotericno <65885798+ezotericno@users.noreply.github.com>
2023-07-31 02:24:15 -0400

    Create cars.py

---
+++ PythonCrashCourse/Chapter 5/cars.py
@@ -0,0 +1,10 @@
+cars = ['audi', 'bmw', 'subaru', 'toyota']
+
+for car in cars:
+  if car == 'bmw':
+    print(car.upper())
+  else:
+    print(car.title())
+
+# Notice the doouble = sign, a single = sign means assignment, a double is an equality checker
+# equality checks are case sensitive, so don't forget to capitalize things and debug
