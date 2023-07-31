2dcbf980cf6f17dcf8799c36fa97d902eb86363b
ezotericno <65885798+ezotericno@users.noreply.github.com>
2023-07-30 23:54:03 -0400

    Created cars.py

---
+++ PythonCrashCourse/Chapter 3/cars.py
@@ -0,0 +1,11 @@
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+cars.sort() # Sorts the list in alphabetical order permanently
+print(cars)
+cars.sort(reverse=True) # Sorts the cars in the reverse order permanently
+
+print(sorted(cars)) # The sorted() function only temporarily changes the list order
+
+cars.reverse() # Reverses the order of the list from its current order, does not sort it in an
+
+len(car) # prints the lenth of the list, actual number of items
+
