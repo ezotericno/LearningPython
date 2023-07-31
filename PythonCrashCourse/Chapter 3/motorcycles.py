dd2962cb5491cb359872927314b46248df6deb17
ezotericno <65885798+ezotericno@users.noreply.github.com>
2023-07-30 23:32:56 -0400

    Created motorcycles.py

---
+++ PythonCrashCourse/Chapter 3/motorcycles.py
@@ -0,0 +1,14 @@
+# This program shows how to add and remove list items
+motorcycles = ['honda', 'suzuki', 'yamaha']
+print(motorcycles)
+
+motorcycles[0] = cfmoto # Changes an item
+print(motorcycles)
+
+motorcycles.append('ducati') # Adds an item to the end of the list
+print(motorcycles)
+
+motorcycles.insert(1, 'kawasaki') # Adds an item at a specific spot in the list
+
+del motorcycles[3] # Deletes a specific item from a list
+
