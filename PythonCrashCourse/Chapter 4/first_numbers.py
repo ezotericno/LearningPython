262eaf2248cb99761f0407eb6a53946382a677bd
ezotericno <65885798+ezotericno@users.noreply.github.com>
2023-07-31 00:10:23 -0400

    Create first_numbers.py

---
+++ PythonCrashCourse/Chapter 4/first_numbers.py
@@ -0,0 +1,5 @@
+for value in range(1, 5):
+  print(value) # Only prints the numbers 1-4, range means inside of the this range but starting at a certain point
+
+numbers = list(range(1, 6))
+print(numbers) # The output will be in a list format
