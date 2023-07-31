d2e63fe1f5c51d25b77dec97bd5f20106d290bb9
ezotericno <65885798+ezotericno@users.noreply.github.com>
2023-07-31 02:30:09 -0400

    Create toppings.py

---
+++ PythonCrashCourse/Chapter 5/toppings.py
@@ -0,0 +1,10 @@
+requested_topping = 'mushrooms'
+
+if requested_topping != 'anchovies':
+  print("Hold the anchovies!")
+
+# we can also use 'and' or 'or' to check for multiple conditions 
+
+requested_toppings = ['mushrooms', 'onions', 'pineapple']
+'mushrooms' in requested_toppings
+# A boolen will be the output if the statement is True or False
