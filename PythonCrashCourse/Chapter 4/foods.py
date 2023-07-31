f9c1d6bf10899cc4c6780d47ec97c8e68a15aedd
ezotericno <65885798+ezotericno@users.noreply.github.com>
2023-07-31 02:09:13 -0400

    Create foods.py

---
+++ PythonCrashCourse/Chapter 4/foods.py
@@ -0,0 +1,20 @@
+my_foods = ['pizza', 'falafel', 'carrot cake']
+friends_foods = my_foods[:] # takes every item from that list
+
+print("my favorite foods are: ")
+print(my_foods)
+
+print("\nMy friend's favorite foods are: ")
+print(friends_foods)
+
+# Example to show we have two separate lists
+my_foods.append('cannoli')
+friends_foods.append('ice cream')
+
+print("my favorite foods are: ")
+print(my_foods)
+
+print("\nMy friend's favorite foods are: ")
+print(friends_foods)
+
+
