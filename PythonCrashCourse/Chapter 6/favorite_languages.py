a7c416e14f717e41ba9702ac8a0d04eb57d2152d
ezotericno <65885798+ezotericno@users.noreply.github.com>
2023-08-01 22:29:52 -0400

    Create favorite_languages.py

---
+++ PythonCrashCourse/Chapter 6/favorite_languages.py
@@ -0,0 +1,11 @@
+# Dictionary of similar objects
+favorite_languages = {
+  'jen': 'python',
+  'sarah': 'c',
+  'edward': 'ruby',
+  'phil': 'python',
+}
+
+# To look for this info you can use the below code
+language = favorite_language['sarah'].title()
+print(f"Sarah's favorite language is {language}.")
