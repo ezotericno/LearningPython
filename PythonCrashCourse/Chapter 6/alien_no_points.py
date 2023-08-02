d7d157f563e66cbbf136e70859ff56d7f64746ec
ezotericno <65885798+ezotericno@users.noreply.github.com>
2023-08-01 23:02:54 -0400

    Create alien_no_points.py

---
+++ PythonCrashCourse/Chapter 6/alien_no_points.py
@@ -0,0 +1,7 @@
+alien_0 = {'color': 'green', 'speed': 'slow'}
+print(alien_0['points'])
+
+# Using get() method to access values
+point_value = alien_0.get('points', 'No point value assigned.')
+point(point_value)
+
