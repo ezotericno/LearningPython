b529428e6816395202ce0087afc10d5bbe93909a
ezotericno <65885798+ezotericno@users.noreply.github.com>
2023-09-13 05:34:11 -0400

    Update remember_me.py

--- PythonCrashCourse/Chapter 10/remember_me.py
+++ PythonCrashCourse/Chapter 10/remember_me.py
@@ -44,17 +44,22 @@ def get_stored_username():
   else:
     return username
 
+def get_new_username():
+  """Prompt for a new username."""
+  username = input("What is your name? ")
+  filename = 'username.json'
+  with open(filename, 'w') as f:
+    json.dump(username, f)
+  return username
+
 def greet_user():
   """Greet the user by name."""
   username = get_stored_username()
   if username:
     print(f"Welcome back, {username}!")
   else:
-    username = input("What is your name? ")
-    filename = 'username.json'
-    with open(filename, 'w') as f:
-      json.dump(username, f)
-      print(f"We'll remember you when you come back, {username}!")
+    username = get_new_username()
+    print(f"We'll remember you when you come back, {username}!")
 
 greet_user()
 
