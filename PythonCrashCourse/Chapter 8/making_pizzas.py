846c2624a849c6cd8279b330280709fedadfcaca
ezotericno <65885798+ezotericno@users.noreply.github.com>
2023-09-12 23:58:38 -0400

    Create making_pizzas.py

---
+++ PythonCrashCourse/Chapter 8/making_pizzas.py
@@ -0,0 +1,8 @@
+# Cont. from pizza.py
+
+import pizza
+
+pizza.make_pizza(16, 'pepperoni')
+pizza.make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
+
+# use this template to import entire modules: module_name.function_name()
