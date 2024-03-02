5a3c6358582c821f473845c6c6f4ca098263406d
Anthony P <65885798+ezotericno@users.noreply.github.com>
2024-03-02 04:56:53 -0500

    Update importing.py

--- CISCO_PyEssentials2/Module 1/importing.py
+++ CISCO_PyEssentials2/Module 1/importing.py
@@ -1,2 +1,18 @@
+import module as alias
+from module import name as alias
+
+import my_module
+result = my_module.my_function(my_module.my_data)
+
+import math as m
+    
+print(m.sin(m.pi/2))
+
 import math
+from math import pi
+from math import sin, pi
+
+print(sin(pi/2))
+
+
 print(math.sin(math.pi/2))
